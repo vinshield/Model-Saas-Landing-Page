9abebc5737f02ac79aa99b3836b2f41c708f5f9c
Jesse <jeslelacodes@gmail.com>
2024-07-23 11:37:51 +0100

    Complete header design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,24 +2,24 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import clsx from "clsx";
-
+import { twMerge } from "tailwind-merge";
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Light Saas Landing Page",
-  description: "Template created by Frontend Tribe",
+	title: "Light Saas Landing Page",
+	description: "Template created by Frontend Tribe",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" className="relative">
-      <body className={clsx(dmSans.className, "antialiased bg-[#EAEEFE]")}>
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en' className='relative'>
+			<body className={clsx(dmSans.className, "antialiased bg-[#EAEEFE]")}>
+				{children}
+			</body>
+		</html>
+	);
 }
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,38 @@
-export const Navbar = () => {
-  return null;
+import ArrowRight from "@/assets/arrow-right.svg";
+import Logo from "@/assets/logosaas.png";
+import Image from "next/image";
+import MenuIcon from "@/assets/menu.svg";
+
+export const Header = () => {
+  return (
+    <header>
+      <div className="flex items-center justify-center gap-3 bg-black py-3">
+        <p className="hidden text-white/60 md:block">
+          Streamline your workflow and boost your productivity
+        </p>
+        <div className="inline-flex items-center gap-1 text-sm text-white">
+          <p>Get started for free</p>
+          <ArrowRight className="h-4 w-4" />
+        </div>
+      </div>
+      <div className="py-5">
+        <div className="container">
+          <div className="flex items-center justify-between">
+            <Image src={Logo} alt="Saas logo" height={40} width={40} />
+            <MenuIcon className="h-4 w-4 md:hidden" />
+            <nav className="hidden items-center gap-5 text-black/60 md:flex">
+              <a href="#">About</a>
+              <a href="#">Features</a>
+              <a href="#">Customers</a>
+              <a href="#">Updates</a>
+              <a href="#">Help</a>
+              <button className="rounded-lg bg-black px-4 py-2 font-medium tracking-tight text-white">
+                Get for free
+              </button>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
