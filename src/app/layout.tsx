df588223e4eed885babb1123e5c376ad96a16cee
Jesse <jeslelacodes@gmail.com>
2024-08-05 21:42:51 +0100

    change metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,11 +5,33 @@ import clsx from "clsx";
 import { twMerge } from "tailwind-merge";
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
+import opengraphimage from "../../opengraph-image.png";
+
 export const metadata: Metadata = {
   title: "Light Saas Landing Page Built With NextJS",
   description:
     "Sample landing page for a SAAS with beautiful gradients and animations to hold user's attention.",
-  openGraph: { type: "website" },
+  openGraph: {
+    title: "Light Saas Landing Page Built With NextJS",
+    description:
+      "Sample landing page for a SAAS with beautiful gradients and animations to hold user's attention.",
+    url: "https://model-saas-landing-page.vercel.app/",
+    images: [
+      //   {
+      //     url: "../../opengraph-image.png", // Must be an absolute URL
+      //     width: 800,
+      //     height: 600,
+      //   },
+      {
+        url: "../../opengraph-image.png", // Must be an absolute URL
+        width: 1200,
+        height: 630,
+        alt: "My custom alt",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
